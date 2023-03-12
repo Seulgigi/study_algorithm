19e819d675c495b3b21e5c4a880f4aa6ada3d935
Seulgigi <slki1205@naver.com>
2023-03-12 22:24:09 +0900

    [23.03.12]

--- silver11659.java
+++ silver11659.java
@@ -3,29 +3,26 @@ import java.util.Scanner;
 public class silver11659 {
 
     // 단순 합계로 구현 시 시간초과
-    public static void add(int start, int end, int[] arr) {
-        
-        int result = 0;
-        for(int i = start - 1; i < end; i++) {
-            result += arr[i];
-        }
-        System.out.println(result);
-    }
+    // 누적 합 값을 배열에 넣기
+
     public static void main(String[] args) {
 
         Scanner sc  = new Scanner(System.in);
 
         int N = sc.nextInt();
         int M = sc.nextInt();
+        int add = 0;
 
-        int[] arr = new int[N];
-        for(int k = 0; k < arr.length; k++) {
-            arr[k] = sc.nextInt();
+        int[] arr = new int[N+1];
+        arr[0] = 0;
+        for(int k = 1; k < arr.length; k++) {
+            add += sc.nextInt();
+            arr[k] = add;
         }
 
         for(int k = 0; k < M; k ++) {
             int i = sc.nextInt(), j = sc.nextInt();
-            add(i, j, arr);
+            System.out.println(arr[j]-arr[i-1]);
         }
     }
 }
